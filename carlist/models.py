644cc371e25d6d0a813891c95ce92e938ec74e9d
Thanh Hai Nguyen <ngu014@gmail.com>
2017-08-19 15:38:42 +0700

    A carjournal is a journal record with following infor: - Journal date: the date that the proposer using the car actually - Proposed date: the date that the proposer return to the quest

--- carlist/models.py
+++ carlist/models.py
@@ -8,7 +8,25 @@ class Car(models.Model):
     car_product_year = models.CharField(max_length = 4)
     car_bought_year = models.CharField(max_length=4)
     car_color = model.CharField(max_length = 15)
-    
+
 # Car journal book
 class CarJournal(models.Model):
-	journal_date = models.DateField()
+    proposed_date = models.DateField()
+    journal_date = models.DateField()
+    odo_start = models.IntegerField(max_length=6)
+    odo_end = models.IntegerField(max_length=6)
+    #Devision that proposing the care
+    proposing_devision = models.CharField(max_length=20)
+    #head of devision that propose the car who approve the car request
+    proposing_approver = models.CharField(max_length=20)
+    #asigned driver
+
+    #Staff who assign this order to driver
+
+    #Head of Administrative devision who approve this order and assignment
+
+    #destination 1, destination 2 ...
+
+    #Expensediture
+
+    
